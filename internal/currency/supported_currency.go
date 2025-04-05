7f511e8bbbc50eceb4b08ebd5a0b9dc273910730
Taufiq Pohan <moonchild@gnuweeb.org>
2025-04-05 17:08:56 +0700

    GetCurrencies: Caching autofill
    Cache result of user's currency filter to make autofill faster and set
    ttl 1 hour.
    
    Signed-off-by: Taufiq Pohan <moonchild@gnuweeb.org>

--- internal/currency/supported_currency.go
+++ internal/currency/supported_currency.go
@@ -1,7 +1,9 @@
 package currency
 
 import (
+	"bytes"
 	"context"
+	"encoding/gob"
 	"encoding/json"
 	"fmt"
 	"sort"
@@ -23,12 +25,13 @@ import (
 //   - Maximum 25 results are returned to comply with Discord limits
 //   - Each option contains currency name and code (e.g. "US Dollar (USD)") and have code as value
 func (c *Currency) GetCurrencies(query string) []*discordgo.ApplicationCommandOptionChoice {
+	rdbCtx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	if len(c.supportedCurrencies) > 0 {
-		return c.filterResult(query, c.supportedCurrencies)
+		return c.filterResult(rdbCtx, query, c.supportedCurrencies)
 	}
 
-	rdbCtx := context.Background()
-	defer rdbCtx.Done()
 	rdbCache, err := c.rdb.Get(rdbCtx, "currencies").Result()
 	if err != redis.Nil {
 		var currencies []*discordgo.ApplicationCommandOptionChoice
@@ -40,7 +43,7 @@ func (c *Currency) GetCurrencies(query string) []*discordgo.ApplicationCommandOp
 		}
 		c.supportedCurrencies = currencies
 
-		return c.filterResult(query, c.supportedCurrencies)
+		return c.filterResult(rdbCtx, query, c.supportedCurrencies)
 	}
 
 	if c.provider == nil {
@@ -77,7 +80,8 @@ func (c *Currency) GetCurrencies(query string) []*discordgo.ApplicationCommandOp
 		return nil
 	}
 	c.supportedCurrencies = currencies
-	return c.filterResult(query, currencies)
+
+	return c.filterResult(rdbCtx, query, currencies)
 }
 
 // filterResult filters and limits currency options based on a search query
@@ -91,14 +95,28 @@ func (c *Currency) GetCurrencies(query string) []*discordgo.ApplicationCommandOp
 //   - If query is empty, returns first 25 items from data
 //   - If query is provided, returns up to 25 matching items
 //   - Items are matched if their name contains the query string (case-insensitive)
-func (c *Currency) filterResult(query string, data []*discordgo.ApplicationCommandOptionChoice) []*discordgo.ApplicationCommandOptionChoice {
+func (c *Currency) filterResult(ctx context.Context, query string, data []*discordgo.ApplicationCommandOptionChoice) []*discordgo.ApplicationCommandOptionChoice {
 	limit := 25 // Maximum options that discord option can store
+	maxLenQueryToBeCached := 2
 	if query == "" {
 		return data[:limit]
 	}
 
 	var result []*discordgo.ApplicationCommandOptionChoice
+	var buff bytes.Buffer
 	query = strings.ToLower(query)
+
+	caches, err := c.rdb.Get(ctx, "currency:autofill:"+query).Bytes()
+	if err != redis.Nil {
+		buff := bytes.NewBuffer(caches)
+		dec := gob.NewDecoder(buff)
+		decErr := dec.Decode(&result)
+		if decErr != nil {
+			utils.ErrorLog.Println("Failed to dencode autofill result:", err)
+			fmt.Println("Failed to dencode autofill result:", err)
+		}
+		return result
+	}
 
 	for _, datum := range data {
 		name := strings.ToLower(datum.Name)
@@ -106,8 +124,26 @@ func (c *Currency) filterResult(query string, data []*discordgo.ApplicationComma
 			result = append(result, datum)
 		}
 	}
-	if len(result) < limit {
+
+	// Convert result into gob and cache it into redis
+	if len(result) > limit {
+		result = result[:limit]
+	}
+	if len(query) < maxLenQueryToBeCached {
 		return result
 	}
-	return result[:limit]
+	enc := gob.NewEncoder(&buff)
+	err = enc.Encode(result)
+	if err != nil {
+		utils.ErrorLog.Println("Failed to encode autofill result:", err)
+		fmt.Println("Failed to encode autofill result:", err)
+	} else {
+		err = c.rdb.SetEx(ctx, "currency:autofill:"+query, buff.Bytes(), time.Hour).Err()
+		if err != nil {
+			utils.ErrorLog.Println("Failed to store cache:", err)
+			fmt.Println("Failed to store cache:", err)
+		}
+	}
+
+	return result
 }
